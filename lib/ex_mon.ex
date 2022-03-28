7e956c5d0923418c58d006eef9a8ddb230ea0b14
frankjony17 <frankjony17@gmail.com>
2022-03-28 18:57:34 -0300

    fix: add game player and info

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,9 +1,15 @@
 
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_game = "Robotinik"
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_game |> create_player(:punch, :kick, :heal) |> Game.start(player)
   end
 
   IO.puts "Started OK..."
