32e91cb7c495815d157a17f95828d3d52696aea6
frankjony17 <frankjony17@gmail.com>
2022-03-29 17:40:52 -0300

    fix: add game attack

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,8 +1,9 @@
 
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.{Actions, Status}
 
-  @computer_game = "Robotinik"
+  @computer_game "Robotinik"
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
@@ -10,7 +11,20 @@ defmodule ExMon do
 
   def start_game(player) do
     @computer_game |> create_player(:punch, :kick, :heal) |> Game.start(player)
+    Status.print_round_message()
   end
 
-  IO.puts "Started OK..."
+  def make_move(move) do
+    move |> Actions.fetch_move() |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "realiza_cura"
+      move -> Actions.attack(move)
+    end
+  end
+
 end
